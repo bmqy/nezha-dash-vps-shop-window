703022dae35ee718f5b5e5266e75074cb9142fa4
bmqy <bmqy@qq.com>
2025-01-09 09:16:36 +0800

    update

--- src/components/VSWServerCardInline.tsx
+++ src/components/VSWServerCardInline.tsx
@@ -39,8 +39,8 @@ export default function ServerCardInline({ now, serverInfo }: { now: number; ser
         )}
         onClick={() => navigate(`/server/${serverInfo.id}`)}
       >
-        <section className={cn("grid items-center gap-2 lg:w-36")} style={{ gridTemplateColumns: "auto auto 1fr" }}>
-          <div className="relative w-30 flex flex-col">
+        <section className={cn("grid items-center gap-2 lg:w-36")}>
+          <div className="relative w-32 flex flex-col">
             <p className={cn("break-all font-bold tracking-tight", showFlag ? "text-xs " : "text-sm")}>
               <span className="h-2 w-2 inline-flex mr-1 shrink-0 rounded-full bg-green-500 self-center"></span>
               {showFlag ? <VSWServerFlag country_code={country_code} /> : null}
