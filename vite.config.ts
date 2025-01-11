f03399e0fb71abbf03f0be2a74f838083a1d81a1
北门清燕 <qq88268459@126.com>
2025-01-12 02:34:14 +0800

    update

--- vite.config.ts
+++ vite.config.ts
@@ -14,53 +14,47 @@ const getGitHash = () => {
 }
 
 // https://vite.dev/config/
-export default defineConfig(()=>{
-  const isSSL = process.env.VITE_NEZHA_DASHBOARD_ENABLE_SSL === "true";
-  const domain = process.env.VITE_NEZHA_DASHBOARD_DOMAIN;
-  console.log(isSSL, typeof process.env.VITE_NEZHA_DASHBOARD_ENABLE_SSL, domain);
-  return {
-    base: "/",
-    define: {
-      "import.meta.env.VITE_GIT_HASH": JSON.stringify(getGitHash()),
+export default defineConfig({
+  base: "/",
+  define: {
+    "import.meta.env.VITE_GIT_HASH": JSON.stringify(getGitHash()),
+  },
+  plugins: [react()],
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
     },
-    plugins: [react()],
-    resolve: {
-      alias: {
-        "@": path.resolve(__dirname, "./src"),
+  },
+  server: {
+    proxy: {
+      "/api/v1/ws/server": {
+        target: "ws://localhost:8008",
+        changeOrigin: true,
+        ws: true,
+      },
+      "/api/v1/": {
+        target: "http://localhost:8008",
+        changeOrigin: true,
       },
     },
-    server: {
-      proxy: {
-        "/api/v1/ws/server": {
-          target: isSSL ? `wss://${domain}` : `ws://${domain}`,
-          changeOrigin: true,
-          rewriteWsOrigin: true,
-          ws: true,
-        },
-        "/api/v1/": {
-          target: isSSL ? `https://${domain}` : `http://${domain}`,
-          changeOrigin: true,
-        },
-      },
-      headers: {
-        "Cache-Control": "no-store",
-        Pragma: "no-cache",
-      },
+    headers: {
+      "Cache-Control": "no-store",
+      Pragma: "no-cache",
     },
-    build: {
-      rollupOptions: {
-        output: {
-          entryFileNames: `assets/[name].[hash].js`,
-          chunkFileNames: `assets/[name].[hash].js`,
-          assetFileNames: `assets/[name].[hash].[ext]`,
-          manualChunks(id) {
-            if (id.includes("node_modules")) {
-              return id.toString().split("node_modules/")[1].split("/")[0].toString()
-            }
-          },
+  },
+  build: {
+    rollupOptions: {
+      output: {
+        entryFileNames: `assets/[name].[hash].js`,
+        chunkFileNames: `assets/[name].[hash].js`,
+        assetFileNames: `assets/[name].[hash].[ext]`,
+        manualChunks(id) {
+          if (id.includes("node_modules")) {
+            return id.toString().split("node_modules/")[1].split("/")[0].toString()
+          }
         },
       },
-      chunkSizeWarningLimit: 1500,
     },
-  }
+    chunkSizeWarningLimit: 1500,
+  },
 })
